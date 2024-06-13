389cb9f2763742ebcca46f7f9236c39be393e2db
kolimisneha <166527564+kolimisneha@users.noreply.github.com>
2024-06-13 18:47:28 +0530

    firts message

--- api/index.js
+++ api/index.js
@@ -56,7 +56,7 @@ app.use("/api/listing", listingRouter);
 app.use(express.static(path.join(__dirname,'/client/dist')));
 
 app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'client','index.html'));
+  res.sendFile(path.join(__dirname,'client','dist','index.html'));
 })
 // Error handling middleware
 app.use((err, req, res, next) => {
